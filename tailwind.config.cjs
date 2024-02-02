2dad63d87e000d5b0baa0363c3a64163ddcde082
Jonathan Trang <jon301@gmail.com>
2024-02-02 16:16:12 +0100

    feat: setup tailwind

---
+++ tailwind.config.cjs
@@ -0,0 +1,10 @@
+/* eslint-env node */
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{html,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};

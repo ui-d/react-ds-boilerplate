5f9f9dc603dd759df787f42697fb51863d473223
ui-d <dawiddeveloper@gmail.com>
2024-02-28 12:16:37 +0100

    chore: tailwind heading config added

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,29 +1,57 @@
-/* eslint-env node */
 import plugin from "tailwindcss/plugin";
 
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html", "./src/**/*.{html,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'interphases': ['"TT Interphases Pro"', 'sans-serif'],
+      },
+      colors: {
+        'content-primary': '#000',
+        'content-secondary': '#707177',
+      },
+    },
   },
   plugins: [
     require("@tailwindcss/container-queries"),
-    plugin(function ({ addBase, addComponents }) {
+    plugin(function ({ addBase, addComponents, theme }) {
       addBase({
         ":root": {
           "--color-accent": "blue",
+          "--content-primary": "var(--content-primary, #000)",
+          "--content-secondary": "var(--content-secondary, #707177)",
         },
         ".dark": {
           "--color-accent": "red",
         },
       });
       addComponents({
-        ".heading-900": {
-          "@apply text-[42px]": {},
-          "@apply font-[700]": {},
-          "@apply leading-[110%]": {},
-          "@apply -tracking-[0.42px]": {},
+        ".heading-1000": {
+          color: "var(--content-primary, #000)",
+          textAlign: "center",
+          fontFamily: theme('fontFamily.interphases'),
+          fontSize: "3.125rem",
+          fontWeight: "700",
+          lineHeight: "110%",
+          letterSpacing: "-0.03125rem",
+        },
+        ".heading-900-semi": {
+          color: "var(--content-primary, #000)",
+          textAlign: "center",
+          fontFamily: theme('fontFamily.interphases'),
+          fontSize: "1.8125rem",
+          fontWeight: "600",
+          lineHeight: "130%",
+          letterSpacing: "-0.01813rem",
+        },
+        ".heading-600-semi": {
+          color: "var(--content-primary, #000)",
+          textAlign: "center",
+          fontFamily: theme('fontFamily.interphases'),
+          fontSize: "1.0625rem",
+          fontWeight: "600",
+          lineHeight: "130%",
         },
       });
     }),
